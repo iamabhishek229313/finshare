b800f658a30ca01cf3c7102310973ebde0a0b40b
Abhishek Kumar <iamabhishek229313@gmail.com>
2021-07-26 04:13:57 +0530

    added few more details

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -77,6 +77,20 @@ class _HomeState extends State<Home> {
               borderRadius: BorderRadius.circular(7),
               elevation: 9.0,
               shadowColor: Colors.black87,
+              // child: CreditCard(
+              //     cardNumber: "5450 7879 4864 7854",
+              //     cardExpiry: "10/25",
+              //     cardHolderName: "Card Holder",
+              //     cvv: "456",
+              //     bankName: "Axis Bank",
+              //     cardType: CardType.masterCard, // Optional if you want to override Card Type
+              //     showBackSide: false,
+              //     frontBackground: CardBackgrounds.black,
+              //     backBackground: CardBackgrounds.white,
+              //     showShadow: true,
+              //     textExpDate: 'Exp. Date',
+              //     textName: 'Name',
+              //     textExpiry: 'MM/YY'),
               child: Container(
                   height: screenHeight * 0.275,
                   decoration: BoxDecoration(
@@ -267,7 +281,11 @@ class _HomeState extends State<Home> {
                                     Expanded(
                                       flex: 2,
                                       child: CircleAvatar(
-                                        child: Text('A'),
+                                        child: Text(
+                                          "A",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        backgroundColor: Colors.black,
                                       ),
                                     ),
                                     Expanded(
